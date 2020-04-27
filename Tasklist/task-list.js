624c61c644c56a881a57ada265e2c52712a91371
macklark <50847468+macklark@users.noreply.github.com>
2020-04-27 16:19:40 +0530

    Added local storage to values

--- Tasklist/task-list.js
+++ Tasklist/task-list.js
@@ -3,7 +3,7 @@ const button1 = document.getElementById('about');
 button1.addEventListener('click', aboutbtn);
 
 function aboutbtn(){
-    window.location.href = "./about_t.html";
+    window.location.href = "./about_btn.html";
 };
 
 const button2 = document.getElementById('donate');
@@ -11,13 +11,41 @@ const button2 = document.getElementById('donate');
 button2.addEventListener('click', donatebtn);
 
 function donatebtn(){
-    window.location.href = "./donate_t.html";
+    window.location.href = "./contact_btn.html";
 };
 
 const button3 = document.getElementById('addbtn');
 const input_t = document.getElementById('first_name');
 const uno = document.querySelector('.collection');
-document.addEventListener('DOMContentLoaded',loadonscreen);
+document.addEventListener("DOMContentLoaded", gettasks);
+
+
+function gettasks(){
+    let tasks;
+    if(localStorage.getItem("tasks") === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach((task) => {
+        const li = document.createElement('li');
+
+        li.className = "collection-item";
+    
+        li.appendChild(document.createTextNode(task));
+    
+        const link = document.createElement('a');
+    
+        link.className = "delete-item secondary-content"
+    
+        link.innerHTML = `<i class="material-icons">clear</i>`;
+    
+        li.appendChild(link);
+    
+        uno.appendChild(li); 
+    })
+}
 
 button3.addEventListener('click', addbtn_t);
 
@@ -25,7 +53,7 @@ function addbtn_t(e){
     if(input_t.value === ""){
         M.toast({html: 'Type something !'})  // This is a toast function inbuilt by materialize.
     
-    e.preventDefault();
+    
 
     }else{
 
@@ -45,40 +73,51 @@ function addbtn_t(e){
 
     uno.appendChild(li);
 
+    storelocal(input_t.value)
+
     input_t.value = "";
 
-    }
 
-    uno.addEventListener('click',remove);
-
-    function remove(e){
-        if(e.target.parentElement.classList.contains('delete-item')){
-            e.target.parentElement.parentElement.remove();
-        }
-    }
-
-    storelocal(input_t.value);
 
     function storelocal(task){
         let tasks;
-        if(localStorage.getItem('tasks') === null){
+        if(localStorage.getItem("tasks") === null){
             tasks = [];
         }else{
             tasks = JSON.parse(localStorage.getItem("tasks"));
         }
-    }
-    tasks.push(task);
 
-    localStorage.setItem('tasks',JSON.stringify("tasks"));
+        tasks.push(task);
 
-    function loadonscreen(){
-        let tasks;
-        if(localStorage.getItem('tasks') === null){
-            tasks = [];
-        }else{
-            tasks = JSON.parse(localStorage.getItem("tasks"));
-        }
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+     }
     }
 
+    e.preventDefault()
+}
 
+uno.addEventListener('click',remove);
+function remove(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        e.target.parentElement.parentElement.remove();
+
+        removeloc(e.target.parentElement.parentElement);
+    }
+}
+
+function removeloc(item){
+    let tasks;
+    if(localStorage.getItem("tasks") === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+
+    tasks.forEach(function(task, index){
+       if(item.textContent === task){
+           tasks.splice(index, 1)
+       }
+    });
+
+    localStorage.setItem("tasks", JSON.stringify(tasks))
 }
